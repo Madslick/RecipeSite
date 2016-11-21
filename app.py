de23ddb4eb9629b1417c1a6c88291fbe67e8654a
Joe <joeyauby@gmail.com>
2016-11-21 00:19:12 -0800

    Input recipe and read recipe working

--- app.py
+++ app.py
@@ -22,7 +22,87 @@ def main():
 
 @app.route("/insert_recipe", methods=["POST", "GET"])
 def insert_recipe():
+    title = request.form['title']
+    ingredients = []
+    amounts = []
+    ingredient_types = []
+    instructions = []
+
+    ing_count = 0
+    instr_count = 0
+
+    for key, value in request.form.items():
+
+        if key.find('ingredient') >= 0:
+            ing_count += 1
+        if key.find('instruction') >= 0:
+            instr_count += 1
+
+
+    for i in range(0, ing_count):
+        ingredients.append(request.form['ingredient' + str(i)])
+        amounts.append(request.form['amount' + str(i)])
+        ingredient_types.append(request.form['ingr_type' + str(i)])
+    for i in range(0, instr_count):
+        instructions.append(request.form['instruction' + str(i)])
+
+    for i in ingredients:
+        print i
+    for i in instructions:
+        print i
+
+    cursor.execute("INSERT INTO Recipe (Title) VALUES (%s)", (title))
+    conn.commit()
+
+    id = cursor.lastrowid
+
+    ingredient_sql = "INSERT INTO Recipe_Ingredients (RecipeId, Name, IngredientTypeId, Amount)" \
+                     " VALUES (%s, '%s', %s, '%s')" % (id, ingredients[0], ingredient_types[0], amounts[0])
+
+    for i in range (1, len(ingredients)):
+        ingredient_sql = ingredient_sql + ", (%s, '%s', %s, '%s')" % (id, ingredients[i], ingredient_types[i], amounts[i])
+    print ingredient_sql
+    cursor.execute(ingredient_sql)
+    conn.commit()
+
+    instruction_sql = "INSERT INTO Recipe_Instructions (RecipeId, Instruction, StepNumber) VALUES (%s, '%s', %s)" % (id, instructions[0], 1)
+
+    for i in range (1, len(instructions)):
+        instruction_sql = instruction_sql + ", (%s, '%s', %s)" % (id, instructions[i], i + 1)
+
+    cursor.execute(instruction_sql)
+
+    conn.commit()
+
     return redirect(url_for('main'))
+
+@app.route("/read_recipe/<string:recipe_title>", methods=['GET'])
+def read_recipe(recipe_title):
+    ingredients = []
+    amounts = []
+    ingredient_types = []
+    instructions = []
+
+    cursor.execute("SELECT Recipe_Ingredients.Name, Amount, Ingredient_Type.Name FROM Recipe_Ingredients "
+                   "INNER JOIN Ingredient_Type ON Ingredient_Type.Id = Recipe_Ingredients.IngredientTypeId "
+                   "INNER JOIN Recipe ON Recipe_Ingredients.RecipeId = Recipe.Id WHERE Recipe.Title = %s"
+                   " ORDER BY Recipe_Ingredients.IngredientTypeId ASC", (recipe_title))
+    results = cursor.fetchall()
+
+
+    for row in results:
+        ingredients.append(row[0])
+        amounts.append(row[1])
+        ingredient_types.append(row[2])
+
+    cursor.execute("SELECT Instruction FROM Recipe_Instructions INNER JOIN Recipe ON Recipe_Instructions.RecipeId = Recipe.Id WHERE Recipe.Title = %s ORDER BY Recipe_Instructions.StepNumber ASC", (recipe_title))
+
+    results = cursor.fetchall()
+
+    for row in results:
+        instructions.append(row[0])
+
+    return render_template("read_recipe.html", Title=recipe_title, recipe_title=recipe_title, ingredients=ingredients, amounts=amounts, ingredient_types=ingredient_types, instructions=instructions)
 
 @app.route("/input_recipe", methods=["POST", "GET"])
 def input_recipe():
--- static/js/add_inputs.js
+++ static/js/add_inputs.js
@@ -1,14 +1,66 @@
-var ingred_num = 0;
-var instr_num = 0;
+var ingred_num = 1;
+var ingr_dropdown_num = 1;
+var instr_num = 1;
+var amount_num = 1;
 
 function add_ingredient(){
     var elem = document.getElementById("ingredients");
+    elem.appendChild(document.createElement('br'));
 
+    var amount = document.createElement('input');
+    amount.type = "text";
+    amount.name = "amount" + amount_num;
+    amount.placeholder = "3 cups";
+    amount_num ++;
+
+    elem.appendChild(amount);
 
     var newIngredient = document.createElement('input');
     newIngredient.type = "text";
-    elem.appendChild(document.createElement('br'));
+    newIngredient.name = "ingredient" + ingred_num;
+    newIngredient.placeholder = "Tofu";
+    ingred_num ++;
+
     elem.appendChild(newIngredient);
+
+    var ingr_dropdown = document.createElement("select");
+    ingr_dropdown.name = "ingr_type" + ingr_dropdown_num;
+    ingr_dropdown_num ++;
+    elem.appendChild(ingr_dropdown);
+
+    var protein_option = document.createElement("option");
+    protein_option.value = "1";
+    protein_option.textContent="Proteins";
+
+
+    var grains_option = document.createElement("option");
+    grains_option.value = "2";
+    grains_option.textContent="Grains/Rice";
+
+    var dry_ingredients_option = document.createElement("option");
+    dry_ingredients_option.value = "3";
+    dry_ingredients_option.textContent = "Dry Ingredients";
+
+    var wet_ingredients_option = document.createElement("option");
+    wet_ingredients_option.value = "4";
+    wet_ingredients_option.textContent = "Wet Ingredients";
+
+    var oils_option = document.createElement("option");
+    oils_option.value = "5";
+    oils_option.textContent = "Oils";
+
+    var seasoning_option = document.createElement("option");
+    seasoning_option.value = "6";
+    seasoning_option.textContent = "Seasoning/Spices";
+
+    ingr_dropdown.appendChild(protein_option);
+    ingr_dropdown.appendChild(grains_option);
+    ingr_dropdown.appendChild(dry_ingredients_option);
+    ingr_dropdown.appendChild(wet_ingredients_option);
+    ingr_dropdown.appendChild(oils_option);
+    ingr_dropdown.appendChild(seasoning_option);
+
+
 };
 
 function add_instruction(){
@@ -16,8 +68,8 @@ function add_instruction(){
 
     var newInstruction = document.createElement('input');
     newInstruction.type = "text";
-    newInstruction.name = "instruction" + toString(instr_num);
-    console.log(toString(newInstruction.name));
+    newInstruction.name = "instruction" + instr_num;
+    instr_num ++;
     elem.appendChild(document.createElement('br'));
     elem.appendChild(newInstruction);
 
